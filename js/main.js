130cd082ed039e76248d2fd1a1ca988b7c5d196c
Firuzjon Xamroev <fkhamroev@MacBook-Pro-Firuzjon.local>
2025-03-06 17:40:20 +0500

    fix : some bugs

--- js/main.js
+++ js/main.js
@@ -77,11 +77,11 @@ document.addEventListener("DOMContentLoaded", function () {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20,
+        spaceBetween: 10,
       },
       768: {
         slidesPerView: 2,
-        spaceBetween: 30,
+        spaceBetween: 20,
       },
       1024: {
         slidesPerView: 3,
